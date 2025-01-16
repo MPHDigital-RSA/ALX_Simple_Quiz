75750ebb0402ac62f478b1b7976b648ee7468a5e
Prince Hlela <mphdigital@protonmail.com>
2025-01-16 10:20:49 +0200

    corrected the feedback message

--- quiz.js
+++ quiz.js
@@ -12,7 +12,7 @@ let checkAnswer = () => {
     let userAnswer = displayRadioValue();
 
     if (userAnswer === correctAnswer){
-        feedback.textContent = "Correct! Well done";
+        feedback.textContent = "Correct! Well done.";
     }else{
         feedback.textContent = "That's incorrect. Try again!";
     }
